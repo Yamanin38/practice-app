585b7d4ddc72ae6dbccfb1a85496122bb10a9bc1
Yamanin38 <yamanin3@outlook.jp>
2026-06-12 06:56:58 +0900

    ギャラリーのページ機能を実装した(バグあり)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,12 +5,13 @@ class PagesController < ApplicationController
   def gallery
     @images = Image.ordered_by_date
     @images = @images.by_date(params[:date]) if params[:date].present?
+    @images = @images.page(params[:page]).per(20)
     @upload_dates = Image.all
                         .map { |img| img.created_at.in_time_zone('Tokyo').to_date }
                         .uniq
                         .sort
                         .reverse
-end
+  end
 
   def articles
   end
