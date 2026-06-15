54a812377f5a42edf6a311c1b7f1e01b86aaae34
Yamanin38 <yamanin3@outlook.jp>
2026-06-16 02:20:38 +0900

    デザインの修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    # 最新記事を日時の降順で最大10件取得
+    @latest_articles = Article.order(created_at: :desc).limit(10)
+
+    # 最新の画像を日時の降順で1件取得（ファイルのアタッチメントも含む）
+    @latest_image = Image.order(created_at: :desc).with_attached_file.first
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   def index
+    # 最新記事を日時の降順で最大10件取得
+    @latest_articles = Article.order(created_at: :desc).limit(10)
+
+    # 最新の画像を日時の降順で1件取得
+    @latest_image = Image.order(created_at: :desc).with_attached_file.first
   end
 
   def gallery
