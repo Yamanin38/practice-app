0054aeb4e3166b78c844049a73a611d3882ff670
Yamanin38 <yamanin3@outlook.jp>
2026-06-17 19:46:32 +0900

    ログアウト処理時にもとにいたページに戻るようにした

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: "ログアウトしました"
+    redirect_back_or_to root_path, notice: "ログアウトしました"
   end
 end
