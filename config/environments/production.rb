73a836e37f22a233f027841e4f577c9b1cbbb19a
Yamanin38 <yamanin3@outlook.jp>
2026-06-26 02:24:11 +0900

    画像を取得できない問題を修正中2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,12 @@ Rails.application.configure do
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
+  # ホスト名を明示的に指定
+config.action_controller.default_url_options = {
+  host: "taninin.com",
+  protocol: "https"
+}
+
   # Ensures that a master key has been made available in ENV["RAILS_MASTER_KEY"], config/master.key, or an environment
   # key such as config/credentials/production.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
